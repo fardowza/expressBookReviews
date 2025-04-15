28069d239e701e1ea9ac9088832524324841bed4
Fardowza Ali <fardowza.a.2@gmail.com>
2025-04-15 09:18:32 -0500

    Adding changes

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,29 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+if(users.find(user => user.username === username)){
+  return false;
+}
+return true;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+if((users.find(user => user.username === username)) && (users.find(user => user.password === password))){
+  return true;
+}
+return false;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username , password } = req.body;
+  if(authenticatedUser(username,password)){
+    const accessToken = jwt.sign({ username}, "SomeNonsense", { expiresIn : '1h'});;
+    return res.status(202).json({message: "Sucessfully logged in", accessToken});
+  }
+  return res.status(401).json({message: "Invalid credentials"});
 });
 
 // Add a book review
